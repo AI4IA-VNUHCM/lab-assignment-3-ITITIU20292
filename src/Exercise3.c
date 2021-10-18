b233c06167ccb559d58fc0d30fba902a2a9b4ec0
ITITIU20292 <ITITIU20292@student.hcmiu.edu.vn>
2021-10-18 17:45:15 +0700

    update ex3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -14,7 +14,40 @@ Ex:
 
 void Ex3(int in_arr[], int n){
 	//Your codes here
-	
+	if(n==0){
+		return;
+	}
+	int array1[n];
+	array1[0]=1;
+	for (int i=1;i<n;i++)
+	{
+		array1[i]=1;
+		if(in_arr[i-1] <in_arr[i]){
+			array1[i]=array1[i-1]+1;
+		}
+	}
+	int array2[n];
+	array2[n-1]=1;
+	for(int i=n-2;i>=0;i--)
+	{
+		array2[i]=1;
+		if(in_arr[i]>in_arr[i+1])
+		{
+			array2[i]=array2[i+1]+1;
+		}
+	}
+
+	int lbs_len=1;
+	int begin=0,end=0;
+	for(int i =0 ;i<n;i++)
+	{
+       if(lbs_len <array1[i]+array2[i] -1){
+		   lbs_len=array1[i]+array2[i]-1;
+		   begin= i -array1[i]+1;
+		   end=i+array2[i] -1;
+	   }
+	}
+	printf("Increasing [%d,%d]",begin,end);
 }
 
 int main(int argc, char *argv[]) {
